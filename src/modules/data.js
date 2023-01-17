9948780a79dc122a493804e2bdac78b8ac290e86
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-17 21:39:22 +0400

    Set up data of list items in an array

---
+++ src/modules/data.js
@@ -0,0 +1,19 @@
+const listItems = [
+  {
+    description: "Take out the trash",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "Do the dishes",
+    completed: true,
+    index: 2,
+  },
+  {
+    description: "Study JavaScript",
+    completed: false,
+    index: 3,
+  },
+];
+
+export default listItems;
