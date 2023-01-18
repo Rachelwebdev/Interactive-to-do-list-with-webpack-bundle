26d68fc960f2e5c691926c7e77760b22dba6ab4d
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-18 22:12:03 +0400

    Delete hardcoded array elements

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,20 +1,3 @@
-const listItems = [
-  {
-    description: 'Take out the trash',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Finish weekly task',
-    completed: false,
-    index: 2,
-  },
-
-  {
-    description: 'Study JavaScript',
-    completed: false,
-    index: 1,
-  },
-];
+const listItems = [];
 
 export default listItems;
