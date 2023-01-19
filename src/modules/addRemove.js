c7f11081ab40b180f9914c9d619b510d659d2623
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-19 11:42:28 +0400

    Create function to add element to the array

--- src/modules/addRemove.js
+++ src/modules/addRemove.js
@@ -1,20 +1,30 @@
+// Imports
 import listItems from "./data.js";
+import listItemDisplay from "./listCard.js";
+
+// Variables declaration
 const inputField = document.getElementById("inputField");
+const kebabMenu = document.querySelector(".fa-ellipsis-vertical");
 
 const addTask = () => {
   let inputValue = inputField.value;
-  console.log(inputValue);
+  if (inputValue.trim() === "") {
+    alert("input is empty");
+    return;
+  }
   listItems.push(inputValue);
   console.log(listItems);
   inputField.value = "";
+  listItemDisplay();
 };
 
 const removeTask = () => {
   let inputValue = inputField.value;
   for (let i = 0; i < listItems.length; i++) {
     if (inputValue === listItems[i]) {
-      listItems.pop();
+      listItems.splice(i, 1);
       console.log(listItems);
+      break;
     }
   }
 };
