9ee3698e30125e99da652fac644e4402cfa8b296
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-21 06:51:46 +0400

    Edit styles for the card

--- src/modules/listCard.js
+++ src/modules/listCard.js
@@ -1,33 +1,74 @@
-// Import statements
-import listItems from "./data.js";
-import { removeTask } from "./addRemove.js";
-import { forEach } from "lodash";
+import { updateLocalStorage, getLocalStorage } from "./data.js";
 
-// Create Variables
+const toDoList = document.querySelector(".toDoContainer");
 
-const toDoListContainer = document.getElementById("to-do-list-container");
-
-const listItemDisplay = () => {
-  toDoListContainer.innerHTML = "";
-  listItems
-    .sort((a, b) => a.index - b.index)
-    .forEach((item) => {
-      toDoListContainer.innerHTML += `
-        <li id="li-element">
-            <input type="checkbox" id="${item.id}" value="${item.completed}" />${item.task}            
-            <i class="fa-solid fa-ellipsis-vertical"></i>
-            <i class="fa-solid fa-trash"></i>        
-          </li>
-      `;
-    });
-  const deleteIcon = document.querySelectorAll(".fa-trash");
-
-  for (let i = 0; i < deleteIcon.length; i += 1) {
-    deleteIcon[i].addEventListener("click", (e) => {
-      removeTask(e.target.parentElement);
-      console.log(listItems);
-    });
-  }
+// Add Task
+export const addTask = (toDoListArray, task) => {
+  toDoListArray.push({ task, completed: false, id: toDoListArray.length + 1 });
+  updateLocalStorage(toDoListArray);
 };
 
-export default listItemDisplay;
+export const renderToDoList = (toDoListArray) => {
+  toDoList.innerHTML = "";
+
+  toDoListArray = getLocalStorage();
+  toDoListArray.forEach((toDo) => {
+    const toDoItem = document.createElement("li");
+    toDoItem.classList.add("toDoContainer-li");
+
+    const toDoCheckbox = document.createElement("input");
+    toDoCheckbox.classList.add("toDoContainer-li-checkbox");
+    toDoCheckbox.type = "checkbox";
+    toDoCheckbox.checked = toDo.completed;
+    toDoItem.appendChild(toDoCheckbox);
+
+    const toDoText = document.createElement("input");
+    toDoText.classList.add("toDoContainer-li-text");
+    toDoText.value = toDo.task;
+    toDoItem.appendChild(toDoText);
+
+    if (toDo.completed) {
+      toDoText.classList.add("completed");
+    }
+    const crossIcon = document.createElement("span");
+    crossIcon.classList.add("trash-can");
+    crossIcon.innerHTML = "🗑️";
+    toDoItem.appendChild(crossIcon);
+    toDoList.appendChild(toDoItem);
+  });
+};
+
+// Edit Task
+export const editTask = (e, toDoListArray) => {
+  const clickedTask = e.target.closest(".toDoContainer-li-text");
+  clickedTask.disabled = false;
+  clickedTask.focus();
+  const taskText = clickedTask.value;
+  clickedTask.addEventListener("keyPress", (e) => {
+    console.log(clickedTask);
+    if (e.key === "Enter" && clickedTask.value !== "") {
+      const taskIndex = toDoListArray.findIndex(
+        (task) => task.task === taskText
+      );
+      toDoListArray[taskIndex].task = clickedTask.value;
+      clickedTask.disabled = true;
+      updateLocalStorage(toDoListArray);
+      renderToDoList(toDoListArray);
+    }
+  });
+};
+
+// Delete Task
+export const deleteTask = (e, toDoListArray) => {
+  const clickedCross = e.target.closest(".trash-can");
+  const clickedTask = clickedCross.previousElementSibling;
+  const taskIndex = toDoListArray.findIndex(
+    (task) => task.task === clickedTask.value
+  );
+  toDoListArray.splice(taskIndex, 1);
+  toDoListArray.forEach((task, index) => {
+    task.id = index + 1;
+  });
+  updateLocalStorage(toDoListArray);
+  renderToDoList(toDoListArray);
+};
