e90635205be9a3cf9cece46b279f138f87f770e2
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-21 08:44:12 +0400

    Add event listeners to clear  button and checkbox

--- src/index.js
+++ src/index.js
@@ -1,48 +1,76 @@
-import './style.css';
+import "./style.css";
 
 import {
   renderToDoList,
   addTask,
+  clearCompleted,
   editTask,
   deleteTask,
-} from './modules/listCard.js';
+  markTask,
+} from "./modules/listCard.js";
 
-import { updateLocalStorage, getLocalStorage as toDoTasks } from './modules/data.js';
+import {
+  updateLocalStorage,
+  getLocalStorage as toDoTasks,
+} from "./modules/data.js";
 
-const input = document.querySelector('.inputField');
-const todoList = document.querySelector('.toDoContainer');
-const addTaskBtn = document.querySelector('.insert-btn');
+// Declare variables
+const input = document.querySelector(".inputField");
+const todoList = document.querySelector(".toDoContainer");
+const addTaskBtn = document.querySelector(".insert-btn");
 
-input.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter' && input.value !== '') {
+const clearCompletedBtn = document.querySelector(".clearbtn");
+
+// ADD TASK EVENT LISTENER FOR KEYPRESS EVENT
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && input.value !== "") {
     addTask(toDoTasks(), input.value);
-    input.value = '';
+    input.value = "";
     renderToDoList(toDoTasks());
   }
 });
 
-addTaskBtn.addEventListener('click', () => {
-  if (input.value !== '') {
+// ADD TASK EVENT LISTENER FOR CLICK EVENT
+
+addTaskBtn.addEventListener("click", () => {
+  if (input.value !== "") {
     addTask(toDoTasks(), input.value);
-    input.value = '';
+    input.value = "";
     updateLocalStorage(toDoTasks());
     renderToDoList(toDoTasks());
   }
 });
 
-todoList.addEventListener('click', (e) => {
-  if (e.target.closest('.toDoContainer-li-text')) {
+// EDIT TASK EVENT LISTENER
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.closest(".toDoContainer-li-text")) {
     editTask(e, toDoTasks());
   }
 });
 
-todoList.addEventListener('click', (e) => {
-  if (e.target.closest('.trash-can')) {
+todoList.addEventListener("click", (e) => {
+  if (e.target.closest(".trash-can")) {
     deleteTask(e, toDoTasks());
   }
 });
 
+// CLEAR COMPLETED EVENT LISTENER
+clearCompletedBtn.addEventListener("click", () => {
+  const toDoList = clearCompleted(toDoTasks());
+  updateLocalStorage(toDoList);
+  renderToDoList(toDoTasks());
+});
+
+// MARK TASK EVENT LISTENER
+todoList.addEventListener("click", (e) => {
+  if (e.target.closest(".toDoContainer-li-checkbox")) {
+    markTask(e, toDoTasks());
+  }
+});
+
 const render = () => {
-  window.addEventListener('load', renderToDoList(toDoTasks()));
+  window.addEventListener("load", renderToDoList(toDoTasks()));
 };
 render();
