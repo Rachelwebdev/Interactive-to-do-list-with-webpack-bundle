613a7b60f56e514813a487d152ebc95553785bc2
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-21 06:53:19 +0400

    Add event Listners to buttons and call  functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 import "./style.css";
-import listItemDisplay from "./modules/listCard.js";
-import { addTask } from "./modules/addRemove.js";
 
-// Variables
-const deleteButton = document.querySelector(".btn-add");
+import {
+  renderToDoList,
+  addTask,
+  editTask,
+  deleteTask,
+} from "./modules/listCard.js";
 
-deleteButton.addEventListener("click", () => {
-  addTask();
+import { updateLocalStorage } from "./modules/data.js";
+import { getLocalStorage as toDoTasks } from "./modules/data.js";
+
+const input = document.querySelector(".inputField");
+const todoList = document.querySelector(".toDoContainer");
+const addTaskBtn = document.querySelector(".insert-btn");
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && input.value !== "") {
+    addTask(toDoTasks(), input.value);
+    input.value = "";
+    renderToDoList(toDoTasks());
+  }
+});
+
+addTaskBtn.addEventListener("click", () => {
+  if (input.value !== "") {
+    addTask(toDoTasks(), input.value);
+    input.value = "";
+    updateLocalStorage(toDoTasks());
+    renderToDoList(toDoTasks());
+  }
+});
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.closest(".toDoContainer-li-text")) {
+    editTask(e, toDoTasks());
+  }
+});
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.closest(".trash-can")) {
+    deleteTask(e, toDoTasks());
+  }
 });
 
 const render = () => {
-  window.addEventListener("load", listItemDisplay());
+  window.addEventListener("load", renderToDoList(toDoTasks()));
 };
 render();
