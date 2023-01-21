8cbd96ee307700a754fb6198864a840b6a28b864
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-21 08:38:17 +0400

    Add Function to mark tasks when complete

--- src/modules/listCard.js
+++ src/modules/listCard.js
@@ -84,3 +84,17 @@ export const clearCompleted = (toDoListArray) => {
   });
   return toDoListArray;
 };
+
+// Marked Complete
+
+export const markTask = (e, toDoListArray) => {
+  const clickedCheckbox = e.target.closest(".toDoContainer-li-checkbox");
+  const clickedTask = clickedCheckbox.nextElementSibling;
+  const taskIndex = toDoListArray.findIndex(
+    (task) => task.task === clickedTask.value
+  );
+  toDoListArray = getLocalStorage();
+  toDoListArray[taskIndex].completed = !toDoListArray[taskIndex].completed;
+  updateLocalStorage(toDoListArray);
+  renderToDoList(toDoListArray);
+};
