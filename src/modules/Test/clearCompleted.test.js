a541eee94a8ae3bbe2959acc80aabdacc3d093c4
Lancelot-SO <fsowah001@gmail.com>
2023-01-26 15:14:13 +0000

    added clearall test

--- src/modules/Test/clearCompleted.test.js
+++ src/modules/Test/clearCompleted.test.js
@@ -1,17 +1,17 @@
-import removeCompleted from '../clearCompleted.js';
-import { addNewTask, ListItems } from '../addItem.js';
+import removeCompleted from "../clearCompleted.js";
+import { addNewTask, ListItems } from "../addItem.js";
 
-describe('test for clearcomplete task', () => {
-  addNewTask('This is first clearcomplete task');
-  addNewTask('This is second clearcomplete task');
-  addNewTask('This is third clearcomplete task');
-  addNewTask('This is fourth clearcomplete task');
-  it('removeCompleted', () => {
+describe("test for clearcomplete task", () => {
+  addNewTask("This is first clearcomplete task");
+  addNewTask("This is second clearcomplete task");
+  addNewTask("This is third clearcomplete task");
+  addNewTask("This is fourth clearcomplete task");
+  it("removeCompleted", () => {
     const complete = removeCompleted(2, true);
     expect(complete).toEqual(ListItems);
   });
 
-  it('completeIndex', () => {
+  it("completeIndex", () => {
     const complete = removeCompleted(3, true);
     expect(complete).toHaveLength(4);
   });
