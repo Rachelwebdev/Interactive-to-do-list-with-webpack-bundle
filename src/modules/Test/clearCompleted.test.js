8242a45ec0fc6113ed513515075971a65cd86fc0
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-26 19:05:58 +0400

    Create test for clear completed tasks function

---
+++ src/modules/Test/clearCompleted.test.js
@@ -0,0 +1,18 @@
+import removeCompleted from "../clearCompleted.js";
+import { addNewTask, ListItems } from "../addItem.js";
+
+describe("test for clearcomplete task", () => {
+  addNewTask("This is first clearcomplete task");
+  addNewTask("This is second clearcomplete task");
+  addNewTask("This is third clearcomplete task");
+  addNewTask("This is fourth clearcomplete task");
+  it("removeCompleted", () => {
+    const complete = removeCompleted(2, true);
+    expect(complete).toEqual(ListItems);
+  });
+
+  it("completeIndex", () => {
+    const complete = removeCompleted(3, true);
+    expect(complete).toHaveLength(4);
+  });
+});
