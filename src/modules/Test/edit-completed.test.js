240f42416c7129a17d267b67f6ee73d3be9bd8da
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-26 19:05:27 +0400

    Create test for edit task function

---
+++ src/modules/Test/edit-completed.test.js
@@ -0,0 +1,16 @@
+import modifyTask from "../editTask.js";
+import { addNewTask, ListItems } from "../addItem.js";
+
+describe("test for edit task", () => {
+  it("modifyTask", () => {
+    addNewTask("This is a new task");
+    addNewTask("This is another task");
+    addNewTask("This is the third task");
+    const edit = modifyTask(1, "This task has been modified");
+    expect(edit).toEqual(ListItems);
+  });
+  it("updatetaskIndex", () => {
+    const edit = modifyTask(2, "This test checks modified tasks");
+    expect(edit).toHaveLength(3);
+  });
+});
