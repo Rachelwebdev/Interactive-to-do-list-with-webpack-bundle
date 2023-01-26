92babcd56a4343bc2168f5a62b6a5b092af9e807
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-26 12:55:14 +0400

    Creat test for add new task function

---
+++ src/modules/Test/add-delete.test.js
@@ -0,0 +1,20 @@
+import { addNewTask } from "../addItem.js";
+import {removeTask}
+// Testing Add Task Function
+describe("add new task to list", () => {
+  const testValue = [
+    { task: "This is the first item", completed: false, id: 1 },
+  ];
+  test("check if addItem function exists", () => {
+    expect(addNewTask).toBeDefined();
+  });
+  test("check if array is the same as test value description", () => {
+    expect(addNewTask("This is the first item")).toEqual(testValue.task);
+  });
+  test("check if array is the same as test value completed", () => {
+    expect(addNewTask("This is the first item")).toEqual(testValue.completed);
+  });
+  test("check if array is the same as test value id", () => {
+    expect(addNewTask("This is the first item")).toEqual(testValue.id);
+  });
+});
