ff446958e48ff605c407e2db4190b700253b850d
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-02 11:44:50 +0400

    Add list data and store in local storage

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,9 +1,36 @@
-export const updateLocalStorage = (tasksArray) => {
-  localStorage.setItem('toDoList', JSON.stringify(tasksArray));
-};
+// export const updateLocalStorage = (toDoListArray) => {
+//   localStorage.setItem("toDoList", JSON.stringify(toDoListArray));
+// };
 
-export const getLocalStorage = () => {
-  const localStorageData = JSON.parse(localStorage.getItem('toDoList')) || [];
-  const tasksArray = localStorageData;
-  return tasksArray;
-};
+// export const getLocalStorage = () => {
+//   const localStorageData = JSON.parse(localStorage.getItem("toDoList")) || [];
+//   const toDoListArray = localStorageData;
+//   return toDoListArray;
+// };
+
+const myToDoList = [
+  {
+    task: "Continue building todo list project",
+    completed: false,
+    id: 0,
+  },
+  {
+    task: "Weekly meal prep",
+    completed: true,
+    id: 1,
+  },
+  {
+    task: "Take an evening walk",
+    completed: true,
+    id: 1,
+  },
+];
+
+const setLocalStorage = localStorage.setItem(
+  "toDoList",
+  JSON.stringify(myToDoList)
+);
+
+const getLocalStorage = localStorage.getItem(JSON.stringify(myToDoList));
+
+export { setLocalStorage, getLocalStorage };
